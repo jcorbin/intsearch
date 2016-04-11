8adb6685fe2485287548fba81ddc3632d7303905
Joshua T Corbin <jcorbin@wunjo.org>
2016-04-10 19:24:12 -0700

    Add multiGen

---
+++ multigen.go
@@ -0,0 +1,53 @@
+package main
+
+type multiGen struct {
+	gens []solutionGen
+}
+
+func (mg multiGen) init(prob *problem, desc string) {
+	for _, gen := range mg.gens {
+		gen.init(prob, desc)
+	}
+}
+
+func (mg multiGen) fix(prob *problem, c rune, v int) {
+	for _, gen := range mg.gens {
+		gen.fix(prob, c, v)
+	}
+}
+
+func (mg multiGen) interColumn(prob *problem, cx [3]rune) {
+	for _, gen := range mg.gens {
+		gen.interColumn(prob, cx)
+	}
+}
+
+func (mg multiGen) initColumn(prob *problem, cx [3]rune, numKnown, numUnknown int) {
+	for _, gen := range mg.gens {
+		gen.initColumn(prob, cx, numKnown, numUnknown)
+	}
+}
+
+func (mg multiGen) solve(prob *problem, neg bool, c rune, c1, c2 rune) {
+	for _, gen := range mg.gens {
+		gen.solve(prob, neg, c, c1, c2)
+	}
+}
+
+func (mg multiGen) choose(prob *problem, c rune) {
+	for _, gen := range mg.gens {
+		gen.choose(prob, c)
+	}
+}
+
+func (mg multiGen) checkFinal(prob *problem, c rune, c1, c2 rune) {
+	for _, gen := range mg.gens {
+		gen.checkFinal(prob, c, c1, c2)
+	}
+}
+
+func (mg multiGen) finish(prob *problem) {
+	for _, gen := range mg.gens {
+		gen.finish(prob)
+	}
+}
