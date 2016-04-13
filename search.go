6a150f56283e55715bdeb5aefa40f50e0521ddef
Joshua T Corbin <jcorbin@wunjo.org>
2016-04-12 23:45:00 -0700

    search: add debug hook

--- search.go
+++ search.go
@@ -4,6 +4,7 @@ type search struct {
 	frontier []*solution
 	traces   map[*solution][]*solution
 	init     func(func(*solution))
+	debug    func(before bool, sol *solution)
 	result   func(*solution, []*solution)
 	// TODO: better metric support
 	metrics struct {
@@ -33,27 +34,26 @@ func (srch *search) emit(sol *solution) {
 
 func (srch *search) step(sol *solution) {
 	srch.metrics.Steps++
-	// fmt.Printf(">>> %v %v\n", 0, sol)
 	if srch.traces != nil {
 		srch.traces[sol] = append(srch.traces[sol], sol.copy())
 	}
+	if srch.debug != nil {
+		srch.debug(true, sol)
+	}
 	sol.step()
-
-	if !sol.done {
-		// fmt.Printf("... %v %v\n", 0, sol)
-		// if _, ok := sol.steps[sol.stepi-1].(storeStep); ok {
-		// 	fmt.Printf("... %v %s\n", 0, sol.letterMapping())
-		// }
-		return
+	if srch.debug != nil {
+		srch.debug(false, sol)
 	}
 
-	srch.frontier = srch.frontier[1:]
-	var trace []*solution
-	if srch.traces != nil {
-		trace = srch.traces[sol]
-		delete(srch.traces, sol)
+	if sol.done {
+		srch.frontier = srch.frontier[1:]
+		var trace []*solution
+		if srch.traces != nil {
+			trace = srch.traces[sol]
+			delete(srch.traces, sol)
+		}
+		srch.result(sol, trace)
 	}
-	srch.result(sol, trace)
 }
 
 func (srch *search) run(maxSteps int) bool {
