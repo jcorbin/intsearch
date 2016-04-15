a9521f2d54f2a3996f2e8a83785612883e7f22d2
Joshua T Corbin <jcorbin@wunjo.org>
2016-04-14 21:33:55 -0700

    Add gogen_test

---
+++ gogen_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func TestGogenSendMoreMoney(t *testing.T) {
+	var (
+		prob problem
+		gg   = goGen{
+			verified: true,
+		}
+		traces = newTraceWatcher()
+		srch   = search{
+			watcher: traces,
+		}
+	)
+
+	if err := prob.plan("send", "more", "money", &gg); err != nil {
+		t.Fatalf("plan failed: %v", err)
+	}
+
+	numGood := 0
+
+	runSearch(
+		&srch,
+		100000,
+		func(emit func(*solution)) {
+			emit(newSolution(&prob, gg.steps, emit))
+		},
+		func(sol *solution) {
+			if sol.err == errVerifyFailed {
+				t.Logf("!!! invalid solution found: %v %v", sol, sol.letterMapping())
+				for i, soli := range traces[sol] {
+					t.Logf("trace[%v]: %v %s", i, soli, soli.letterMapping())
+				}
+				t.Fail()
+			} else if sol.err != nil {
+				// normal dead end result, discard
+				return
+			} else {
+				numGood++
+			}
+		})
+
+	if numGood == 0 {
+		t.Fatalf("didn't find any solution")
+	} else if numGood > 1 {
+		t.Fatalf("found too many solutions: %v", numGood)
+	}
+}
