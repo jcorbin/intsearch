e0d87a9ed61faec70ebfb8ef0bbe54c9cb004da4
Joshua T Corbin <jcorbin@wunjo.org>
2016-04-17 22:31:59 -0700

    gogen_steps: trivial cleanup

--- gogen_steps.go
+++ gogen_steps.go
@@ -2,101 +2,54 @@ package main
 
 import "fmt"
 
-type saveStep struct {
-}
+type saveStep struct{}
+type restoreStep struct{}
 
-func (step saveStep) String() string {
-	return fmt.Sprintf("save")
-}
+func (step saveStep) String() string    { return fmt.Sprintf("save") }
+func (step restoreStep) String() string { return fmt.Sprintf("restore") }
 
-func (step saveStep) run(sol *solution) {
-	sol.rb = sol.ra
-}
-
-type restoreStep struct {
-}
-
-func (step restoreStep) String() string {
-	return fmt.Sprintf("restore")
-}
-
-func (step restoreStep) run(sol *solution) {
-	sol.ra = sol.rb
-}
+func (step saveStep) run(sol *solution)    { sol.rb = sol.ra }
+func (step restoreStep) run(sol *solution) { sol.ra = sol.rb }
 
 type setStep int
 
-func (v setStep) String() string {
-	return fmt.Sprintf("set(%v)", int(v))
-}
-
-func (v setStep) run(sol *solution) {
-	sol.ra = int(v)
-}
+func (step setStep) String() string    { return fmt.Sprintf("set(%v)", int(step)) }
+func (step setStep) run(sol *solution) { sol.ra = int(step) }
 
 type addStep byte
-
-func (c addStep) String() string {
-	return fmt.Sprintf("add(%s)", string(c))
-}
-
-func (c addStep) run(sol *solution) {
-	sol.ra += sol.values[c]
-}
-
 type subStep byte
-
-func (c subStep) String() string {
-	return fmt.Sprintf("sub(%s)", string(c))
-}
-
-func (c subStep) run(sol *solution) {
-	sol.ra -= sol.values[c]
-}
-
 type divStep int
+type modStep int
 
-func (v divStep) String() string {
-	return fmt.Sprintf("div(%v)", int(v))
-}
+func (step addStep) String() string { return fmt.Sprintf("add(%s)", string(step)) }
+func (step subStep) String() string { return fmt.Sprintf("sub(%s)", string(step)) }
+func (step divStep) String() string { return fmt.Sprintf("div(%v)", int(step)) }
+func (step modStep) String() string { return fmt.Sprintf("mod(%v)", int(step)) }
 
-func (v divStep) run(sol *solution) {
+func (step addStep) run(sol *solution) { sol.ra += sol.values[step] }
+func (step subStep) run(sol *solution) { sol.ra -= sol.values[step] }
+func (step divStep) run(sol *solution) {
 	if sol.ra < 0 {
-		sol.ra = -sol.ra / int(v)
+		sol.ra = -sol.ra / int(step)
 	} else {
-		sol.ra = sol.ra / int(v)
+		sol.ra = sol.ra / int(step)
 	}
 }
+func (step modStep) run(sol *solution) { sol.ra = (sol.ra + int(step)<<1) % int(step) }
 
-type negateStep struct {
-}
+type negateStep struct{}
 
-func (step negateStep) String() string {
-	return fmt.Sprintf("negate")
-}
+func (step negateStep) String() string    { return fmt.Sprintf("negate") }
+func (step negateStep) run(sol *solution) { sol.ra = -sol.ra }
 
-func (step negateStep) run(sol *solution) {
-	sol.ra = -sol.ra
-}
+type exitStep struct{ err error }
 
-type exitStep struct {
-	err error
-}
-
-func (step exitStep) String() string {
-	return fmt.Sprintf("exit(%v)", step.err)
-}
-
-func (step exitStep) run(sol *solution) {
-	sol.exit(step.err)
-}
+func (step exitStep) String() string    { return fmt.Sprintf("exit(%v)", step.err) }
+func (step exitStep) run(sol *solution) { sol.exit(step.err) }
 
 type storeStep byte
 
-func (c storeStep) String() string {
-	return fmt.Sprintf("store(%s)", string(c))
-}
-
+func (c storeStep) String() string { return fmt.Sprintf("store(%s)", string(c)) }
 func (c storeStep) run(sol *solution) {
 	if sol.used[sol.ra] {
 		sol.exit(errAlreadyUsed)
@@ -106,47 +59,27 @@ func (c storeStep) run(sol *solution) {
 }
 
 type relJZStep int
-
-func (o relJZStep) String() string {
-	return fmt.Sprintf("jz(%+d)", int(o))
-}
-
-func (o relJZStep) run(sol *solution) {
-	if sol.ra == 0 {
-		sol.stepi += int(o)
-	}
-}
-
 type relJNZStep int
 
-func (o relJNZStep) String() string {
-	return fmt.Sprintf("jnz(%+d)", int(o))
-}
+func (step relJZStep) String() string  { return fmt.Sprintf("jz(%+d)", int(step)) }
+func (step relJNZStep) String() string { return fmt.Sprintf("jnz(%+d)", int(step)) }
 
-func (o relJNZStep) run(sol *solution) {
-	if sol.ra != 0 {
-		sol.stepi += int(o)
+func (step relJZStep) run(sol *solution) {
+	if sol.ra == 0 {
+		sol.stepi += int(step)
 	}
 }
-
-type modStep int
-
-func (v modStep) String() string {
-	return fmt.Sprintf("mod(%v)", int(v))
-}
-
-func (v modStep) run(sol *solution) {
-	sol.ra = (sol.ra + int(v)<<1) % int(v)
+func (step relJNZStep) run(sol *solution) {
+	if sol.ra != 0 {
+		sol.stepi += int(step)
+	}
 }
 
 type forkUntilStep int
 
-func (v forkUntilStep) String() string {
-	return fmt.Sprintf("forkUntil(%v)", int(v))
-}
-
-func (v forkUntilStep) run(sol *solution) {
-	if sol.ra < int(v) {
+func (step forkUntilStep) String() string { return fmt.Sprintf("forkUntil(%v)", int(step)) }
+func (step forkUntilStep) run(sol *solution) {
+	if sol.ra < int(step) {
 		sol.fork(sol.ra + 1)
 	}
 }
