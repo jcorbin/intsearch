cf2994a6dab280d291c9252377c1a85575787422
Joshua T Corbin <jcorbin@wunjo.org>
2016-05-10 23:20:28 -0700

    main: dump both pre- and post-compile

--- main.go
+++ main.go
@@ -135,11 +135,25 @@ func main() {
 	}
 
 	gg.planProblem.plan(gen)
+
+	if *dumpProg {
+		fmt.Println()
+		fmt.Printf("//// Final Program Dump\n")
+		for i, step := range gg.steps {
+			label := gg.labelFor(i)
+			if label == "" {
+				fmt.Printf("%v: %v\n", i, step)
+			} else {
+				fmt.Printf("%v: %v  // :%s\n", i, step, label)
+			}
+		}
+	}
+
 	gg.compile()
 
 	if *dumpProg {
 		fmt.Println()
-		fmt.Printf("//// Program Dump\n")
+		fmt.Printf("//// Compiled Program Dump\n")
 		for i, step := range gg.steps {
 			label := gg.labelFor(i)
 			if label == "" {
