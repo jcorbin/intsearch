adf9b566a90f2023efe33cd8866087d6c944a75e
Joshua T Corbin <jcorbin@wunjo.org>
2016-06-15 12:33:17 -0700

    runnable.SearchPlan: unpack init

--- runnable/search.go
+++ runnable/search.go
@@ -10,35 +10,23 @@ type SearchPlan struct {
 // Run runs the generated steps.
 func (sp *SearchPlan) Run(res word.Resultor) {
 	run := searchRun{
-		init: func(emit EmitFunc) int {
-			emit(newSolution(&sp.PlanProblem.Problem, sp.steps, emit))
-			// worst case, we have to run every step for every possible brute force solution
-			numBrute := fallFact(sp.Base, len(sp.Letters))
-			return numBrute * len(sp.steps)
-		},
+		// large upper limit for the search execution: run every step for every
+		// possible brute force solution
+		maxSteps: fallFact(sp.Base, len(sp.Letters)) * len(sp.steps),
 		result: func(sol *Solution) bool {
 			res.Result(sol)
 			return false
 		},
 		counter: 0,
 	}
+	run.expand(newSolution(&sp.PlanProblem.Problem, sp.steps, run.expand))
 	run.run()
 }
-
-// EmitFunc is a state-sink function that should will the passed solution for
-// eventual exploration.
-type EmitFunc func(*Solution)
 
 // ResultFunc is a terminal state processing function.  If the result function
 // retains a reference to the solution, it should return true; otherwise the
 // solution object will be re-used after the result function finishes.
 type ResultFunc func(*Solution) bool
-
-// InitFunc is a search initialization function.  It should call the passed
-// emit function at least once to prime the frontier.  It is expected to return
-// the maximum number of steps allowed before a search run is terminated on
-// grounds of insanity.
-type InitFunc func(EmitFunc) int
 
 type search struct {
 	frontier []*Solution
@@ -57,14 +45,12 @@ func (srch *search) expand(sol *Solution) {
 
 type searchRun struct {
 	search
-	init     InitFunc
 	result   ResultFunc
 	maxSteps int
 	counter  int
 }
 
 func (srch *searchRun) run() bool {
-	srch.maxSteps = srch.init(srch.expand)
 	for sol := srch.current(); sol != nil; sol = srch.current() {
 		for sol.Step() {
 			srch.counter++
