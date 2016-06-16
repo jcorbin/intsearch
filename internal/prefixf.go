ca324df5e21eda6ce5147e29f293227b25b508a0
Joshua T Corbin <jcorbin@wunjo.org>
2016-06-16 08:32:39 -0700

    Add internal.PrefixedF

---
+++ internal/prefixf.go
@@ -0,0 +1,49 @@
+package internal
+
+import "fmt"
+
+// PrefixedF wraps a formatting function and returns a formatting function that
+// prefixes the format string passed to it with one of the provided prefix
+// strings.  The returned function is stateful such that:
+// - the first call to it will use the first prefix
+// - the second call will use the second prefix,
+// - and so forth, with the last prefixing being used for all subsequent calls;
+//   in other wrds there is no wrap-around or re-use of prefixes, the final
+//   prefix "sticks"
+//
+// For example:
+//     myf = PrefixedF(fmt.Printf, "hello", "world", "meh...)
+//     myf("such\n")
+//     myf("much\n")
+//     myf("amaze\n")
+//     myf("still amazing\n")
+//
+// Would output:
+//     hello such
+//     world much
+//     meh... amaze
+//     meh... still amazing
+func PrefixedF(
+	logf func(string, ...interface{}),
+	prefixes ...string,
+) func(string, ...interface{}) {
+	return (&prefixf{
+		logf:     logf,
+		prefixes: prefixes,
+	}).outf
+}
+
+type prefixf struct {
+	logf     func(string, ...interface{})
+	prefixes []string
+	i        int
+}
+
+func (pf *prefixf) outf(format string, args ...interface{}) {
+	pfx := pf.prefixes[pf.i]
+	if pf.i < len(pf.prefixes)-1 {
+		pf.i++
+	}
+	format = fmt.Sprintf("%s %s", pfx, format)
+	pf.logf(format, args...)
+}
