e31ce977899df8d977d303152494f4e633aa8882
Joshua T Corbin <jcorbin@wunjo.org>
2016-06-17 21:52:51 -0700

    Generalize gen_test support

--- runnable/gen_test.go
+++ runnable/gen_test.go
@@ -1,138 +1,49 @@
 package runnable_test
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
-	"github.com/jcorbin/intsearch/internal"
+	"github.com/jcorbin/intsearch/internal/gen_testing"
 	"github.com/jcorbin/intsearch/runnable"
 	"github.com/jcorbin/intsearch/word"
 )
 
+func stepGenF(prob *word.PlanProblem) word.SolutionGen {
+	return runnable.NewStepGen(prob)
+}
+
 func TestStepGen_prunedBrute(t *testing.T) {
-	runStepGenTest(t, word.PlanPrunedBrute, "send", "more", "money")
+	gen_testing.RunGenTest(t, stepGenF, word.PlanPrunedBrute, "send", "more", "money")
 }
 
 func BenchmarkStepGenPlan_prunedBrute(b *testing.B) {
-	benchStepGenPlan(b, word.PlanPrunedBrute, "send", "more", "money")
+	gen_testing.BenchGenPlan(b, stepGenF, word.PlanPrunedBrute, "send", "more", "money")
 }
 
 func BenchmarkStepGenRun_prunedBrute(b *testing.B) {
-	benchStepGenRun(b, word.PlanPrunedBrute, "send", "more", "money")
+	gen_testing.BenchGenRun(b, stepGenF, word.PlanPrunedBrute, "send", "more", "money")
 }
 
 func TestStepGen_bottomUp(t *testing.T) {
-	runStepGenTest(t, word.PlanBottomUp, "send", "more", "money")
+	gen_testing.RunGenTest(t, stepGenF, word.PlanBottomUp, "send", "more", "money")
 }
 
 func BenchmarkStepGenPlan_bottomUp(b *testing.B) {
-	benchStepGenPlan(b, word.PlanBottomUp, "send", "more", "money")
+	gen_testing.BenchGenPlan(b, stepGenF, word.PlanBottomUp, "send", "more", "money")
 }
 
 func BenchmarkStepGenRun_bottomUp(b *testing.B) {
-	benchStepGenRun(b, word.PlanBottomUp, "send", "more", "money")
+	gen_testing.BenchGenRun(b, stepGenF, word.PlanBottomUp, "send", "more", "money")
 }
 
 func TestStepGen_topDown(t *testing.T) {
-	runStepGenTest(t, word.PlanTopDown, "send", "more", "money")
+	gen_testing.RunGenTest(t, stepGenF, word.PlanTopDown, "send", "more", "money")
 }
 
 func BenchmarkStepGenPlan_topDown(b *testing.B) {
-	benchStepGenPlan(b, word.PlanTopDown, "send", "more", "money")
+	gen_testing.BenchGenPlan(b, stepGenF, word.PlanTopDown, "send", "more", "money")
 }
 
 func BenchmarkStepGenRun_topDown(b *testing.B) {
-	benchStepGenRun(b, word.PlanTopDown, "send", "more", "money")
-}
-
-func runStepGenTest(t *testing.T, planf word.PlanFunc, w1, w2, w3 string) {
-	var (
-		prob word.Problem
-		plan word.Plan
-	)
-	if err := prob.Setup(w1, w2, w3); err != nil {
-		t.Fatalf("setup failed: %v", err)
-	}
-
-	logf := func(format string, args ...interface{}) {
-		dec := plan.Decorate(args)
-		if len(dec) > 0 {
-			format = fmt.Sprintf("%s  // %s", format, strings.Join(dec, ", "))
-		}
-		t.Logf(format, args...)
-	}
-
-	gg := runnable.NewStepGen(word.NewPlanProblem(&prob, false))
-	plan = planf(gg, true)
-
-	numGood := 0
-	plan.Run(word.ResultFunc(func(sol word.Solution) bool {
-		err := sol.Check()
-		if _, is := err.(word.VerifyError); is {
-			sol.Dump(internal.PrefixedF(logf, "!!! invalid solution found:", "..."))
-			t.Fail()
-		} else if sol.Check() == nil {
-			numGood++
-		}
-		return false
-	}))
-	if numGood == 0 {
-		t.Logf("didn't find any solution")
-		t.Fail()
-	} else if numGood > 1 {
-		t.Logf("found too many solutions: %v", numGood)
-		t.Fail()
-	}
-
-	if t.Failed() {
-		gg = runnable.NewStepGen(word.NewPlanProblem(&prob, true))
-		plan = planf(word.MultiGen([]word.SolutionGen{
-			word.NewLogGen(gg.PlanProblem),
-			gg,
-		}), true)
-		plan.Run(word.NewDebugWatcher(logf))
-	}
-}
-
-func benchStepGenPlan(b *testing.B, planf word.PlanFunc, w1, w2, w3 string) {
-	var prob word.Problem
-	if err := prob.Setup(w1, w2, w3); err != nil {
-		b.Fatalf("setup failed: %v", err)
-	}
-	for n := 0; n < b.N; n++ {
-		gg := runnable.NewStepGen(word.NewPlanProblem(&prob, false))
-		planf(gg, false)
-	}
-}
-
-func benchStepGenRun(b *testing.B, planf word.PlanFunc, w1, w2, w3 string) {
-	var (
-		prob word.Problem
-		plan word.Plan
-	)
-	if err := prob.Setup(w1, w2, w3); err != nil {
-		b.Fatalf("setup failed: %v", err)
-	}
-
-	gg := runnable.NewStepGen(word.NewPlanProblem(&prob, false))
-	plan = planf(gg, false)
-
-	for n := 0; n < b.N; n++ {
-		numGood := 0
-		plan.Run(word.ResultFunc(func(sol word.Solution) bool {
-			if sol.Check() == nil {
-				numGood++
-			}
-			return false
-		}))
-		if numGood == 0 {
-			b.Fatalf("didn't find any solution")
-		} else if numGood > 1 {
-			b.Fatalf("found too many solutions: %v", numGood)
-		}
-		if b.Failed() {
-			break
-		}
-	}
+	gen_testing.BenchGenRun(b, stepGenF, word.PlanTopDown, "send", "more", "money")
 }
